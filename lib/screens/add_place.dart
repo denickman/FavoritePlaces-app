31dec88e61559c20249959778f4b70e5576b4ebf
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-28 17:03:01 +0300

    Add LocationInput into AddPlaceScreen

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -2,6 +2,7 @@ import 'package:favoriteplaces/providers/user_places.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favoriteplaces/widgets/image_input.dart';
+import 'package:favoriteplaces/widgets/location_input.dart';
 import 'dart:io';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
@@ -24,7 +25,9 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addPlace(enteredTitle, _selectedImage!);
     Navigator.of(context).pop();
   }
 
@@ -51,9 +54,15 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             const SizedBox(height: 16),
 
             // image input
-            ImageInput(onPickImage: (selectedImage) {
-              _selectedImage = selectedImage;
-            }),
+            ImageInput(
+              onPickImage: (selectedImage) {
+                _selectedImage = selectedImage;
+              },
+            ),
+
+            const SizedBox(height: 16),
+
+            LocationInput(),
 
             const SizedBox(height: 16),
 
