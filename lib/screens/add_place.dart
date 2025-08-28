3b8229852e37e86eeb2925083baf97db40e8ee18
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-28 14:16:21 +0300

    Show full image in details screen

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -2,6 +2,7 @@ import 'package:favoriteplaces/providers/user_places.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:favoriteplaces/widgets/image_input.dart';
+import 'dart:io';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -14,16 +15,16 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle);
-
+    ref.read(userPlacesProvider.notifier).addPlace(enteredTitle, _selectedImage!);
     Navigator.of(context).pop();
   }
 
@@ -48,10 +49,14 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
             ),
 
             const SizedBox(height: 16),
+
             // image input
-            ImageInput(),
+            ImageInput(onPickImage: (selectedImage) {
+              _selectedImage = selectedImage;
+            }),
 
             const SizedBox(height: 16),
+
             ElevatedButton.icon(
               onPressed: _savePlace,
               icon: const Icon(Icons.add),
