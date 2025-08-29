39fa10827656bf6d6378dc887e3f01ce425f8c2a
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-29 10:37:27 +0300

    Add ability to make an image of your current location

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,8 @@
+import 'package:favoriteplaces/models/place_location.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -11,8 +14,18 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  Location? _pickedLocation;
+
+  PlaceLocation? _pickedLocation;
   var _isGettingLocation = false;
+
+  String get locationImage {
+    if (_pickedLocation == null) {
+      return '';
+    }
+    final lat = _pickedLocation!.latitude;
+    final lng = _pickedLocation!.longitude;   
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyAClcKXsdoKRhoXyMsq1KYLr7x7Za79Tec';
+  }
 
   void _getCurrentLocation() async {
     Location location = Location();
@@ -43,16 +56,38 @@ class _LocationInputState extends State<LocationInput> {
 
     locationData = await location.getLocation();
 
+    // send and translate latitude / longitude in a human-readable address
+    // https://developers.google.com/maps/documentation/geocoding/requests-reverse-geocoding
+    // https://developers.google.com/maps/documentation/maps-static/overview
+
+    final lat = locationData.latitude;
+    final lon = locationData.longitude;
+
+    if (lat == null || lon == null) {
+      return;
+    }
+
+    final url = Uri.parse(
+      'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&key=AIzaSyAClcKXsdoKRhoXyMsq1KYLr7x7Za79Tec',
+    );
+    final response = await http.get(url);
+
+    final resData = json.decode(response.body);
+    final address = resData['results'][0]['formatted_address'];
+
     setState(() {
+      _pickedLocation = PlaceLocation(
+        latitude: lat,
+        longitude: lon,
+        address: address,
+      );
       _isGettingLocation = false;
     });
-
-    print(locationData.latitude);
-    print(locationData.longitude);
   }
 
   @override
   Widget build(BuildContext context) {
+    
     Widget previewContent = Text(
       "No location chosen",
       textAlign: TextAlign.center,
@@ -60,6 +95,15 @@ class _LocationInputState extends State<LocationInput> {
         color: Theme.of(context).colorScheme.onSurface,
       ),
     );
+
+    if (_pickedLocation != null) {
+      previewContent = Image.network(
+        locationImage, 
+        fit: BoxFit.cover, 
+        width: double.infinity, 
+        height: double.infinity
+        );
+    }
 
     if (_isGettingLocation) {
       previewContent = const CircularProgressIndicator();
@@ -98,4 +142,77 @@ class _LocationInputState extends State<LocationInput> {
       ],
     );
   }
-}
+} 
+
+
+
+/*
+  {
+   "results" : [
+      {
+         "address_components" : [
+            {
+               "long_name" : "277",
+               "short_name" : "277",
+               "types" : [ "street_number" ]
+            },
+            {
+               "long_name" : "Bedford Avenue",
+               "short_name" : "Bedford Ave",
+               "types" : [ "route" ]
+            },
+            {
+               "long_name" : "Williamsburg",
+               "short_name" : "Williamsburg",
+               "types" : [ "neighborhood", "political" ]
+            },
+            {
+               "long_name" : "Brooklyn",
+               "short_name" : "Brooklyn",
+               "types" : [ "sublocality", "political" ]
+            },
+            {
+               "long_name" : "Kings",
+               "short_name" : "Kings",
+               "types" : [ "administrative_area_level_2", "political" ]
+            },
+            {
+               "long_name" : "New York",
+               "short_name" : "NY",
+               "types" : [ "administrative_area_level_1", "political" ]
+            },
+            {
+               "long_name" : "United States",
+               "short_name" : "US",
+               "types" : [ "country", "political" ]
+            },
+            {
+               "long_name" : "11211",
+               "short_name" : "11211",
+               "types" : [ "postal_code" ]
+            }
+         ],
+         "formatted_address" : "277 Bedford Avenue, Brooklyn, NY 11211, USA",
+         "geometry" : {
+            "location" : {
+               "lat" : 40.714232,
+               "lng" : -73.9612889
+            },
+            "location_type" : "ROOFTOP",
+            "viewport" : {
+               "northeast" : {
+                  "lat" : 40.7155809802915,
+                  "lng" : -73.9599399197085
+               },
+               "southwest" : {
+                  "lat" : 40.7128830197085,
+                  "lng" : -73.96263788029151
+               }
+            }
+         },
+         "place_id" : "ChIJd8BlQ2BZwokRAFUEcm_qrcA",
+         "types" : [ "street_address" ]
+      },
+
+  ... Additional <code>results[]</code> ...
+*/
