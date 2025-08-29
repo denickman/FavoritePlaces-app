d7f8881bd517568c6bc9c1a415a6f3cc1b4d40c3
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-29 15:16:24 +0300

    Add pick on map feature

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -23,6 +23,8 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  LatLng? _pickedLocation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,24 +35,37 @@ class _MapScreenState extends State<MapScreen> {
         actions: [
           // save picked location
           if (widget.isSelecting)
-            IconButton(icon: const Icon(Icons.save), onPressed: () {}),
+            IconButton(icon: const Icon(Icons.save), onPressed: () {
+              Navigator.of(context).pop(_pickedLocation);
+            }),
         ],
       ),
 
       body: GoogleMap(
+        onTap: !widget.isSelecting ? null : (position) {
+          setState(() {
+            _pickedLocation = position;
+          });
+        },
         initialCameraPosition: CameraPosition(
           target: LatLng(widget.location.latitude, widget.location.longitude),
           zoom: 16,
         ),
-        markers: {
-          Marker(
-            markerId: const MarkerId('m1'),
-            position: LatLng(
-              widget.location.latitude,
-              widget.location.longitude,
-            ),
-          ),
-        },
+        markers: (_pickedLocation == null && widget.isSelecting)
+            ? {
+                // do not show any markers here in that case
+              }
+            : {
+                Marker(
+                  markerId: const MarkerId('m1'),
+                  position:
+                      _pickedLocation ??
+                      LatLng(
+                        widget.location.latitude,
+                        widget.location.longitude,
+                      ),
+                ),
+              },
       ),
     );
   }
