795ec2c10875117df400b53f0e04b070df66d1a6
Dennis Yaremenko <yaremdennis@gmail.com>
2025-08-29 17:14:38 +0300

    Connect userPlacesProvider with PlacesScreen

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -13,8 +13,45 @@ import 'package:path_provider/path_provider.dart' as syspath;
 // отвечает за манипуляции с самим путём (имена файлов, склейка, расширения).
 import 'package:path/path.dart' as path;
 
+Future<Database> _getDatabase() async {
+  // Возвращает путь к стандартной системной директории, где должны храниться базы данных SQLite
+  // (на iOS и Android у каждой платформы своё место).
+  final dbPath = await sql.getDatabasesPath();
+
+  // Открывает (или создаёт структуру таблицы, если ещё не существует) базу данных SQLite.
+  return await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
+      );
+    },
+    version: 1,
+  );
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+
+  void loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('user_places');
+
+    final places = data.map(
+      (row) => Place(
+        id: row['id'] as String,
+        title: row['title'] as String,
+        image: File(row['image'] as String),
+        location: PlaceLocation(
+          latitude: row['lat'] as double,
+          longitude: row['lon'] as double,
+          address: row['address'] as String,
+        ),
+      ),
+    ).toList();
+
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDirectory = await syspath.getApplicationDocumentsDirectory();
@@ -28,20 +65,7 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
       location: location,
     );
 
-// Возвращает путь к стандартной системной директории, где должны храниться базы данных SQLite 
-// (на iOS и Android у каждой платформы своё место).
-    final dbPath = await sql.getDatabasesPath();
-
-// Открывает (или создаёт структуру таблицы, если ещё не существует) базу данных SQLite.
-    final db = await sql.openDatabase(
-      path.join(dbPath, 'places.db'),
-      onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
-        );
-      },
-      version: 1,
-    );
+    final db = await _getDatabase();
 
     /*
       Добавляет новую строку в таблицу user_places.
@@ -50,13 +74,13 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
     */
 
     db.insert('user_places', {
-      'id': newPlace.id, 
+      'id': newPlace.id,
       'title': newPlace.title,
       'image': newPlace.image.path,
       'lat': newPlace.location.latitude,
       'lng': newPlace.location.longitude,
-      'address': newPlace.location.address
-      });
+      'address': newPlace.location.address,
+    });
 
     state = [newPlace, ...state];
   }
